c2841bd434dddd1af769a62f864d71c6e3b98ca8
Anastasiya Pozyomina <snegirev.vladimir.sergeevich@gmail.com>
2019-10-31 16:10:41 +0700

    edits

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,33 @@
-$('.slider').slick();
+$('.slider').slick(
+    
+);
+
+
+
+
+
+
+
+
+var $button = $('#menu-btn');
+
+$button.on('click', function (e) {
+    e.preventDefault();
+    if ($button.hasClass('open')) {
+        $button.removeClass('open');
+        $button.addClass('close');
+    } else {
+        $button.removeClass('close');
+        $button.addClass('open');
+    }
+});
+
+document.querySelector('#menu-btn').onclick = function () {
+    display = document.querySelector('.menu').style.display;
+
+    if (display == 'block') {
+        document.querySelector('.menu').style.display = 'none';
+    } else {
+        document.querySelector('.menu').style.display = 'block';
+    }
+}
